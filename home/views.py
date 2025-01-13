dae551b2098e4bd042446faf6f4ae3d6df9a2791
Perola16 <perola.ppc@gmail.com>
2025-01-13 11:57:55 -0300

    teste

--- home/views.py
+++ home/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("A view index funcionou, Wow!")
+
+def sobre(request):
+    return HttpResponse("<h1>Sistema 1.0 desenvolvido por mim</h1>")
