d0111ea3432a776f05f761be4347ec0a17263bdf
Perola16 <perola.ppc@gmail.com>
2025-02-07 11:10:03 -0300

    07/02/2025

--- home/views.py
+++ home/views.py
@@ -32,20 +32,43 @@ def dados(request):
         'CPF': "******",
         'telefone': "(89)98102-3839",
         'email': "perola.ppc@gmail.com",
-        'endereco': "Rua Domingos Borges, 2367, Santa Fé, Teresina, Piauí,*********",
+        'rua': "Rua Domingos Borges", 
+        'numero': 2367, 
+        'bairro': "Santa Fé", 
+        'cidade': "Teresina", 
+        'estado': "Piauí", 
+        'CEP': "*********",
     }
     return render(request,"dados.html",context)
 
 def form(request):
     if request.method == "POST":
         nome=request.POST.get("nome")
-        idade=request.POST.get("idade")
+        data_nascimento=request.POST.get("data_nascimento")
+        rg=request.POST.get("rg")
+        CPF=request.POST.get("CPF")
+        telefone=request.POST.get("telefone")
+        email=request.POST.get("email")
+        rua=request.POST.get("rua")
+        numero=request.POST.get("numero")
+        bairro=request.POST.get("bairro")
         cidade=request.POST.get("cidade")
+        estado=request.POST.get("estado")
+        CEP=request.POST.get("CEP")
         
         context={
             "nome": nome,
-            "idade": idade,
+            "data_nascimento": data_nascimento,
+            "rg": rg,
+            "CPF": CPF,
+            "telefone": telefone,
+            "email": email,
+            "rua": rua,
+            "numero": numero,
+            "bairro": bairro,
             "cidade": cidade,
+            "estado": estado,
+            "CEP": CEP,
         }
         
         return render(request,"dados.html",context)
